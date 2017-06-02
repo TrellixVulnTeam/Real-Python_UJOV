8f13548de121ed682b416b5cb6eb00c44b3c2236
Raymond Rupiah <rupiray@yahoo.com>
2017-06-02 04:50:32 +0100

    updates

--- flasktaskr/project/db_create.py
+++ flasktaskr/project/db_create.py
@@ -1,25 +1,17 @@
 # sets and creates the database
 
-import sqlite3
-from _config import DATABASE_PATH
+from views import db
+from models import Task
+from datetime import date
 
-with sqlite3.connect(DATABASE_PATH) as connection:
 
-    # get a cursor object used to execute SQL commands
-    c = connection.cursor()
 
-    # create a table
-    c.execute(""" CREATE TABLE tasks(task_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL, due_date TEXT NOT NULL, priority INTEGER NOT NULL,
-        status INTEGER NOT NULL)""")
+# create the database and the db table
+db.create_all()
 
-    # insert dummy data into the table
-    c.execute(
-        'INSERT INTO tasks (name, due_date, priority, status)'
-        'VALUES("Finish this tutorial", "07/20/2017", 10, 1)'
-    )
+#insert data
+db.session.add(Task("Finish all assignments", date(2017, 8, 21), 10, 1))
+db.session.add(Task("Go for a haircut", date(2017, 11, 12), 10, 1))
 
-    c.execute(
-        'INSERT INTO tasks (name, due_date, priority, status)'
-        'VALUES("Finish work stuff", "05/23/2017", 10, 1)'
-    )
+# commit the changes
+db.session.commit()
