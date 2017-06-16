b6322368fa7e30812892beb64006c76eddde3776
Raymond Rupiah <rupiray@yahoo.com>
2017-06-16 01:20:24 +0100

    updated

--- flasktaskr/db_create.py
+++ flasktaskr/db_create.py
@@ -1,8 +1,8 @@
 # sets and creates the database
-from datetime import date
+# from datetime import date
 
 from project import db
-from project.models import Task, User
+# from project.models import Task, User
 
 
 
@@ -11,15 +11,15 @@ from project.models import Task, User
 db.create_all()
 
 #insert data
-db.session.add(
-    User("admin", "admin.com" "admin", "admin")
-)
-db.session.add(
-    Task("Finish all assignments", date(2017, 8, 21), 10, date(2015, 2, 13), 1, 1)
-)
-db.session.add(
-    Task("Go for a haircut", date(2017, 11, 12), 10, date(2015, 2, 13), 1, 1)
-)
+# db.session.add(
+#     User("admin", "admin.com" "admin", "admin")
+# )
+# db.session.add(
+#     Task("Finish all assignments", date(2017, 8, 21), 10, date(2015, 2, 13), 1, 1)
+# )
+# db.session.add(
+#     Task("Go for a haircut", date(2017, 11, 12), 10, date(2015, 2, 13), 1, 1)
+# )
 
 # commit the changes
 db.session.commit()
